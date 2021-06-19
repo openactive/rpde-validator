ac7524a38c6e21356efd2ea73cb92c74e96c7dab
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2021-06-19 16:37:25 +0100

    Fixed typos and improved errors

--- src/rules/page/next-page-base-url-match-current-page-rule.js
+++ src/rules/page/next-page-base-url-match-current-page-rule.js
@@ -11,11 +11,11 @@ const NextUrlBasePathMatchCurrentPage = class extends RpdeRule {
     super();
     this.meta = {
       name: 'NextUrlBasePathMatchCurrentPage',
-      description: 'Validates that the bath path of the next parameter is equal to the bath path of the current page',
+      description: 'Validates that the base path of the next parameter is equal to the base path of the current page',
       tests: {
         default: {
-          description: 'Raises a failure if the bath path of the next parameter is not equal to the bath path of the current page',
-          message: 'The bath path of the `next` property `"{{nextBasePath}}"` must be equal to the bath path of the page `"{{currentBasePath}}"`.',
+          description: 'Raises a failure if the base path of the next parameter is not equal to the base path of the current page',
+          message: 'The base path of the `next` property `"{{nextBasePath}}"` does not match the base path of the RPDE page `"{{currentBasePath}}"`. Please ensure that the `next` url on each page in the feed is correctly referencing the appropriate feed.',
           sampleValues: {
             nextBasePath: 'http://example.com/',
             currentBasePath: 'https://example.com/',
@@ -38,7 +38,7 @@ const NextUrlBasePathMatchCurrentPage = class extends RpdeRule {
     const basePathCurrentPage = getBasePath(node.url);
     const basePathNextPage = getBasePath(node.data.next);
 
-    // The bath path of the next URL must always be equal to the bath path of the current page
+    // The base path of the next URL must always be equal to the base path of the current page
     if (basePathCurrentPage !== basePathNextPage) {
       node.log.addPageError(
         node.url,
