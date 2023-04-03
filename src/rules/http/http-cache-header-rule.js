36d45bce08c01d93d20d7726af1e790b8b869418
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2023-04-03 16:30:12 +0100

    Fixes to error messages returned

--- src/rules/http/http-cache-header-rule.js
+++ src/rules/http/http-cache-header-rule.js
@@ -11,11 +11,11 @@ const HttpCacheHeaderRule = class extends RpdeRule {
     super();
     this.meta = {
       name: 'HttpCacheHeaderRule',
-      description: 'Validates that the Cache-Control returned from the server includes "public", and "max-age" with a value of either 8 (for last page) or 3600 (otherwise) for open data feeds, and not for Orders feeds',
+      description: 'Validates that for open data feeds (and not for Orders feeds) the Cache-Control header returned from the server includes "public", and "max-age" has a value of either 8 (for last page) or 3600 (otherwise)',
       tests: {
         default: {
-          description: 'Raises a warning if the Cache-Control returned from the server for any page of an open data feed before the last page is not "public", and "max-age" with a value of at least 3600',
-          message: 'Response header `Cache-Control` should include "public", and include "max-age" of 3600 or more for all pages except the last page of the feed, actual header returned was `"{{cacheControl}}"`',
+          description: 'Raises a warning if the Cache-Control header returned from the server for any page of an open data feed before the last page is not "public", and has a "max-age" with a value of at least 3600',
+          message: 'Response header `Cache-Control` should include "public", and also include a "max-age" of 3600 or more for all pages except the last page of the feed. Actual header returned was `"{{cacheControl}}"`',
           sampleValues: {
             cacheControl: 'public, max-age=3600',
           },
@@ -24,8 +24,8 @@ const HttpCacheHeaderRule = class extends RpdeRule {
           type: RpdeErrorType.MISSING_CACHE_CONTROL,
         },
         lastPage: {
-          description: 'Raises a warning if the Cache-Control returned from the server for the last page of an open data feed is not "public", and "max-age" with a value of 8 or less',
-          message: 'Response header `Cache-Control` should include "public", and include "max-age" of 8 or less for the last page of the feed, actual header returned was `"{{cacheControl}}"`',
+          description: 'Raises a warning if the Cache-Control header returned from the server for the last page of an open data feed is not "public", and has a "max-age" with a value of 8 or less',
+          message: 'Response header `Cache-Control` should include "public", and also include a "max-age" of 8 or less for the last page of the feed. Actual header returned was `"{{cacheControl}}"`',
           sampleValues: {
             cacheControl: 'public, max-age=8',
           },
@@ -34,8 +34,8 @@ const HttpCacheHeaderRule = class extends RpdeRule {
           type: RpdeErrorType.MISSING_CACHE_CONTROL,
         },
         ordersFeed: {
-          description: 'Raises a failure if the Cache-Control returned from the server for an Orders feed is "public" with any value of "max-age"',
-          message: 'Response header `Cache-Control` should not be "public" or include "max-age" for an Open Booking API Orders Feeds, actual header returned was `"{{cacheControl}}"`',
+          description: 'Raises a failure if the Cache-Control header returned from the server for an Orders feed is "public" with any value of "max-age"',
+          message: 'Response header `Cache-Control` should not be "public" or include "max-age" for Open Booking API Orders Feeds. Actual header returned was `"{{cacheControl}}"`',
           sampleValues: {
             cacheControl: 'public, max-age=8',
           },
